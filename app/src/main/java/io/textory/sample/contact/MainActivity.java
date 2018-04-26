485a7c338209954deef790aae5a170881ab4bd05
sean <hyunsik-park@textory.com>
2018-04-26 15:35:45 +0900

    daily

--- app/src/main/java/io/textory/sample/contact/MainActivity.java
+++ app/src/main/java/io/textory/sample/contact/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        functional.android.contact.all(this, ContactsContract.Contacts.CONTENT_URI, null, null, functional.log::e);
+        functional.contact.all(this, ContactsContract.Contacts.CONTENT_URI, null, null, functional.log::e);
     }
 }
